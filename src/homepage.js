32d44fe1a21fea7cc05f4187272b8ff0abb9d735
nikolovskipetar <petarnikolovski@hotmail.com>
2020-02-21 12:49:54 +0100

    create ecommerce react app

---
+++ src/homepage.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import './homepage.scss';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        <h1 className='title'>SNEAKERS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        <h1 className='title'>T-SHIRTS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        <h1 className='title'>MAN</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        <h1 className='title'>WOMAN</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+
+export default Homepage;
